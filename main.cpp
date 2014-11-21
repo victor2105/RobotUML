aeb7790d60e6f911a5d6ad835f1e5bbe8ae2fc7d
Guillaume BORG <guillaume.unice@gmail.com>
2014-11-21 13:31:43 +0100

    ne pas envoyer

--- main.cpp
+++ main.cpp
@@ -8,6 +8,72 @@ using namespace std;
 
 int main(){
 
+	string cmd, direction;
+	Robot robot;
+	int x, y;
+
+			cout << "----------------------------------------------" << endl;
+			cout << "Robot" << endl;
+			cout << "Position : " << robot.position.getx() <<","<<robot.position.gety() << endl;
+			cout << "Direction :" << robot.direction << endl;
+			cout << "Objet : "; if(robot.aObjet) cout << "Oui" << endl; else cout << endl;
+			cout << "----------------------------------------------" << endl;
+
+			cin >> cmd ;
+
+	do{
+
+		cout << cmd << endl;
+
+		if(cmd == "avancer"){
+			cin >> x >> y ;
+			robot.avancer(x,y);
+		}
+		if(cmd == "tourner"){
+			cin >> direction ;
+			robot.tourner(direction);
+		}
+		if(cmd == "rencontrerPlot"){
+			int plot;
+			cin >> plot ;
+			robot.rencontrerPlot(Plot(plot));
+		}
+
+		if(cmd == "evaluerPlot"){
+			cout << robot.evaluerPlot() << endl ;
+		}
+
+		if(cmd == "peser"){
+			cout << robot.peser() << endl;
+		}
+
+		if(cmd == "saisir"){
+			int poids;
+			cin >> poids ;
+			robot.saisir(Object(poids));
+		}
+
+		if(cmd == "figer"){
+			robot.figer();
+		}
+
+		if(cmd == "repartir"){
+			robot.repartir();
+		}
+
+		if(cmd == "poser"){
+			robot.poser();
+		}
+
+		cout << "----------------------------------------------" << endl;
+		cout << "Robot" << endl;
+		cout << "Position : " << robot.position.getx() <<","<<robot.position.gety() << endl;
+		cout << "Direction :" << robot.direction << endl;
+		cout << "Objet : "; if(robot.aObjet) cout << "Oui" << endl; else cout << endl;
+		cout << "----------------------------------------------" << endl;
+
+	}while(cin >> cmd);
+/*
 	Afficheur_A aff;
 	Afficheur_A aff2;
     //un faux bloc pour limiter la portée
@@ -20,6 +86,7 @@ int main(){
 	      
 		cout << "test" << endl; 
 		aff.change(45);
-	}
+	}*/
 	return 0;
 }
+
